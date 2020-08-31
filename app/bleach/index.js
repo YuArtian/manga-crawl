76d6b2f59587c878a735f1d5f5782773574e1e45
Yu <312364705@qq.com>
2020-08-31 10:43:29 +0800

    死神bleach -done

--- app/bleach/index.js
+++ app/bleach/index.js
@@ -1,5 +1,4 @@
 const cheerio = require('cheerio')
-const axios = require("axios");
 const path = require('path')
 const fs = require('fs')
 const request = require('../request')
@@ -16,66 +15,73 @@ const book_3_list = book.hasPart[2].hasPart.map(i => i.url)
 
 const html_list = [...book_1_list, ...book_2_list, ...book_3_list]
 
-/* 获取图片 */
-const request_img = async img_url =>
-  axios({
-    url: img_url,
-    method: "get",
-    responseType: "stream",
-  });
-
-/* 从 html 中获取图片列表 */
-const get_img_list = html => {
+/* 从 html 中获取图片url列表 */
+const get_img_url_list = html => {
   try {
+    // 加载 html
     const $ = cheerio.load(html)
-    const img_data_html = $('body > script').html()
-    const img_data_base = img_data_html.match(/'(\S*)';/)[0]
-    const img_data_code = new Buffer.from(img_data_base, 'base64').toString()
-    const img_data_list = JSON.parse(img_data_code)
+    // 图片信息
     const vg_r_data = $('.vg-r-data')
     const img_host = vg_r_data.data('host');
     const img_pre = vg_r_data.data('img_pre');
-
-    return img_data_list.map(i => `${img_host}${img_pre}${i.img}`)
+    // 图片列表
+    const img_list_html = $('body > script').html()
+    const img_list_base64 = img_list_html.match(/'(\S*)';/)[0]
+    const img_list = JSON.parse(new Buffer.from(img_list_base64, 'base64').toString())
+    // 图片 url 列表
+    return img_list.map(i => `${img_host}${img_pre}${i.img}`)
   } catch (error) {
-    console.log('ERROR:: get_img_list', error)
+    console.log('ERROR:: get_img_list\r\n', error)
   }
 }
 
 /* 写入文件 */
-const writeFile = (res, section, fileName, index) => {
+const writeFile = (img_stream, html_name, img_name) => {
   return new Promise((resolve, reject) => {
-    const distFileName = path.resolve(__dirname, `../dist/bleach/${section}/${fileName}`)
+    const distFileName = path.resolve(__dirname, `../../dist/bleach/${html_name}/${img_name}.jpg`)
     const stream = fs.createWriteStream(distFileName, { encoding: 'binary' });
-    res.data.pipe(stream);
+    img_stream.pipe(stream);
     stream.on("finish", resolve);
     stream.on("error", reject);
   });
 };
 
+// main
 (async function(){
   try {
-    for (let i = 0; i < html_list.length; i++) {
-      const html_url = html_list[i];
-      await fs.promises.mkdir(path.resolve(__dirname, `../../dist/bleach/第${i+1}回`), { recursive: true })
+    console.log('=== main start ===\r\n')
+    // 根据 html_list 获取 html 文件
+    const html_length = html_list.length
+    for (let html_i = 0; html_i < 1; html_i++) {
+      const html_url = html_list[html_i];
+      const html_name = `第${html_i + 1}回`
+      // 创建章节目录
+      console.log(`创建章节目录:: ${html_name}\r\n`)
+      await fs.promises.mkdir(path.resolve(__dirname, `../../dist/bleach/${html_name}`), { recursive: true })
       // 获取 HTML
+      console.log(`HTML:: 开始下载 -${html_name}\r\n`)
       // url: 'https://www.manhuadb.com/manhua/141/5558_92149_p1.html',
       const html = await request('GET', html_url)
-      const img_list = get_img_list_info(html)
-
+      console.log(`HTML:: 完成下载 -${html_name}\r\n`)
+      // 每一章的图片列表
+      const img_url_list = get_img_url_list(html)
+      // 依次下载图片并保存本地
+      const img_length = img_url_list.length
+      // 根据 img_url_list 获取并保存图片
+      console.log('')
+      for (let img_i = 0; img_i < 10; img_i++) {
+        const img_url = img_url_list[img_i];
+        const img_name = img_i + 1
+        // 下载图片
+        console.log(`IMG:: 开始下载 -${html_name} 第${img_name}\r\n${img_url}\r\n`)
+        const img_stream = await request('GET', img_url, { responseType: "stream" })
+        console.log(`IMG:: 完成下载 -${html_name} 第${img_name}\r\n`)
+        // 写入本地
+        await writeFile(img_stream, html_name, img_name)
+        console.log(`${html_name}:: 写入${img_name}图片成功`)
+      }
     }
-
-
-
-    const length = img_data_list.length
-    for (let index = 0; index < length; index++) {
-      const { img, p } = img_data_list[index];
-      console.log('`${img_host}${img_pre}${img_name}`',`${img_host}${img_pre}${img}`)
-      const res = await request_img(`${img_host}${img_pre}${img}`)
-      await writeFile(res, 1, `${p}.jpg`)
-    }
-
   } catch (error) {
-    console.log('ERROR:: main',error)
+    console.log('ERROR:: main\r\n',error)
   }
 })()
